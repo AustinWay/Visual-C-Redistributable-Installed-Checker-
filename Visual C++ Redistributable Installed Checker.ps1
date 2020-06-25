6a72a5f665117f0522a49bd3c7ddc09ebfbcecab
AustinWay <31394469+AustinWay@users.noreply.github.com>
2020-06-25 11:21:47 -0500

    Update Visual C++ Redistributable Installed Checker.ps1

--- Visual C++ Redistributable Installed Checker.ps1
+++ Visual C++ Redistributable Installed Checker.ps1
@@ -1,6 +1,6 @@
 <#
 Program: Visual C++ Redistributable Installed Checker 
-Description: Checks if Visual C++ Redistributatbles are installed on a local machine
+Description: Powershell script that checks if Visual C++ Redistributables are installed on a local machine
 Author: Austin Way
 Date: 6/25/2020
 #>
